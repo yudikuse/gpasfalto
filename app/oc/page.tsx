f4578a6015ab98aff898eb95ee9a3563e2a43aa4
yudikuse <marcelokuse@gmail.com>
2025-12-12 13:32:24 -0300

    Update page.tsx

--- app/oc/page.tsx
+++ app/oc/page.tsx
@@ -526,7 +526,7 @@ export default function OcPage() {
             src="/gpasfalto-logo.png"
             alt="GP Asfalto"
             style={{
-              height: 72,
+              height: 240,
               width: "auto",
               display: "block",
               margin: "0 auto 8px",
